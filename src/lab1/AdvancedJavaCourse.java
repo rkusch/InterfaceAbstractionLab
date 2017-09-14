0f415cafd6a896ad9f3b5b09d8bb6f7b82dae27f
rkusch <rkusch@10.18.12.1>
2017-09-14 15:37:11 -0500

    1

---
+++ src/lab1/AdvancedJavaCourse.java
@@ -0,0 +1,30 @@
+package lab1;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class AdvancedJavaCourse extends Course{
+
+    public AdvancedJavaCourse(String courseName, String courseNumber) {
+        setCourseName(courseName);
+        setCourseNumber(courseNumber);
+    }
+
+
+    @Override
+    public void setCredits(double credits) {
+        if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        setCredits(credits);
+    }
+
+    
+}
---
+++ src/lab1/IntroJavaCourse.java
@@ -0,0 +1,28 @@
+package lab1;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class IntroJavaCourse extends Course{
+  
+
+    public IntroJavaCourse(String courseName, String courseNumber) {
+        setCourseName(courseName);
+        setCourseNumber(courseNumber);
+    }
+
+
+        @Override
+        public void setCredits(double credits) {
+        if(credits < 0 || credits > 5.0) {
+            System.out.println(
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.setCredits(credits);
+    }
+
+}
