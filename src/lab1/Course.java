e22c932f83a53d67e39ee0dabbc73b610b189445
kusch <kusch@DESKTOP-7BO39IO.attlocal.net>
2017-09-17 14:18:09 -0500

    change gui outputs to exceptions

---
+++ src/lab1/Course.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rkusch
+ */
+public abstract class Course {
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+    private String prerequisites;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public final void setCourseName(String courseName) {
+        if (courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
+
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+      public final void setCourseNumber(String courseNumber) {
+        if (courseNumber == null || courseNumber.length() == 0) {
+            throw new IllegalArgumentException("Error: courseNumber cannot be null of empty string");
+        }
+        this.courseNumber = courseNumber;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+       public void setCredits(double credits) {
+        if (credits < 0.5 || credits > 4.0) {
+            throw new IllegalArgumentException("Error: credits must be in the range 0.5 to 4.0");
+        }
+        this.credits = credits;
+    }
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+    
+    
+    
+}
